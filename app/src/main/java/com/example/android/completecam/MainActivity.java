7f70c74c498a9a042551d90b11cc8525cfde120b
vishwa prakash mishra <vishwaprakashdec1995@gmail.com>
2017-10-04 11:37:56 +0530

    added upper level layout

--- app/src/main/java/com/example/android/completecam/MainActivity.java
+++ app/src/main/java/com/example/android/completecam/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.completecam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     TextView tv = (TextView) findViewById(R.id.sample_text);
     tv.setText(stringFromJNI());
     }
+    TextView camera = (TextView) findViewById(R.id.camera_text_view);
+    String message = camera.getText().toString();
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
