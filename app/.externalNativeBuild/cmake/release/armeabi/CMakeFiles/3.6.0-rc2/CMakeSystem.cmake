64123379617ad8e5e710b7b197e719008d46ea99
vishwa prakash mishra <vishwaprakashdec1995@gmail.com>
2017-11-05 16:19:27 +0530

    changed the source compilation platform
    compilation platform changed to windows NT.

--- app/.externalNativeBuild/cmake/release/armeabi/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ app/.externalNativeBuild/cmake/release/armeabi/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -1,9 +1,9 @@
-set(CMAKE_HOST_SYSTEM "Linux-4.4.0-93-generic")
-set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "4.4.0-93-generic")
-set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+set(CMAKE_HOST_SYSTEM "Windows-10.0.15063")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.15063")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
 
-include("/home/vishwa/Android/ndk-bundle/build/cmake/android.toolchain.cmake")
+include("C:/Users/vishwa/AppData/Local/Android/Sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
 
 set(CMAKE_SYSTEM "Android-21")
 set(CMAKE_SYSTEM_NAME "Android")
